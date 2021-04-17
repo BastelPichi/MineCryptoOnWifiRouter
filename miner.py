2f200d4a68971efc92218fda4d7a2414cb23a36a
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2021-04-17 16:09:13 +0200

    Add files via upload

--- miner.py
+++ miner.py
@@ -7,6 +7,7 @@ import sys  # Only python3 included libraries
 import time
 import urllib.request
 
+
 soc = socket.socket()
 soc.settimeout(10)
 
@@ -86,11 +87,11 @@ while True:
                     feedback = soc.recv(1024).decode().rstrip("\n")
                     # If result was good
                     if feedback == "GOOD":
-                        file = open('/sys/class/leds/fritz4040:amber:wlan/brightness','w')
+                        file = open('/sys/class/leds/fritz4040:amber:wlan/brightness','w')# replace that with the led no. 1 name
                         file.write('1')
                         file.close()
                         time.sleep(0.3)
-                        file = open('/sys/class/leds/fritz4040:amber:wlan/brightness','w')
+                        file = open('/sys/class/leds/fritz4040:amber:wlan/brightness','w')# replace that with the led no. 1 name
                         file.write('0')
                         file.close()
                         print("Accepted share",
@@ -103,11 +104,11 @@ while True:
                         break
                     # If result was incorrect
                     elif feedback == "BAD":
-                        file = open('/sys/class/leds/fritz4040:red:wlan/brightness','w')
+                        file = open('/sys/class/leds/fritz4040:red:wlan/brightness','w')# replace that with the led no. 2 name
                         file.write('1')
                         file.close()
                         time.sleep(0.3)
-                        file = open('/sys/class/leds/fritz4040:red:wlan/brightness','w')
+                        file = open('/sys/class/leds/fritz4040:red:wlan/brightness','w')# replace that with the led no. 2 name
                         file.write('0')
                         file.close()
                         print("Rejected share",
