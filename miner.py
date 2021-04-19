b78c6d9c1a4074fe04bcff2a8311a3ccbc1b76f7
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2021-04-19 18:06:06 +0200

    Update miner.py

--- miner.py
+++ miner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Minimal version of Duino-Coin PC Miner, useful for developing own apps. Created by revox 2020-2021
+# Minimal version of Duino-Coin PC Miner. Created by revox 2020-2021, adapted for openwrt by BastelPichi
 import hashlib
 import os
 import socket
